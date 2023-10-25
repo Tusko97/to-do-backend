d21dd73c76556408b09f807dd95ed1f5ecbef9fa
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-10-25 18:21:53 +0300

    cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://to-do-server-reox.onrender.com'
+    origins 'https://strong-raindrop-be683c.netlify.app/', 'https://to-do-server-reox.onrender.com'
 
     resource "*",
       headers: :any,
